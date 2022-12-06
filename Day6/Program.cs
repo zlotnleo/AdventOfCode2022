6aee94d1ff427e4a4153eb152971473850a76d32
Leo Zlotnikov <Leo.Zlotnikov@softwire.com>
2022-12-06 09:25:19 +0000

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day6;
+
+public class Program
+{
+    private static int Solve(string signal, int markerLength) =>
+        Enumerable.Range(markerLength, signal.Length - markerLength + 1)
+            .First(i => signal[(i - markerLength)..i].Distinct().Count() == markerLength);
+
+    public static void Main()
+    {
+        var signal = File.ReadAllText("input.txt");
+        Console.WriteLine(Solve(signal, 4));
+        Console.WriteLine(Solve(signal, 14));
+    }
+}
